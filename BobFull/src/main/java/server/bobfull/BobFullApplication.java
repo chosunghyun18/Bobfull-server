4588b39443604c0c54c4b8c533e60d6a87a3d02f
chosunghyun18 <krismon@naver.com>
2023-06-23 19:11:26 +0900

    chore : Apply gitignore

---
+++ BobFull/src/main/java/server/bobfull/BobFullApplication.java
@@ -0,0 +1,13 @@
+package server.bobfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobFullApplication.class, args);
+    }
+
+}
---
+++ BobFull/src/test/java/server/bobfull/BobFullApplicationTests.java
@@ -0,0 +1,13 @@
+package server.bobfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
